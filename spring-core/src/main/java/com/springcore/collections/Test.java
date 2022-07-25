baa67928f924cbc02a727a1674f310a6b7d02e97
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-07-25 08:12:28 +0530

    Properties injection with List, Set, Map

---
+++ spring-core/src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,20 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+		Emp emp1=(Emp) context.getBean("emp1");
+		
+		System.out.println(emp1.getName());
+		System.out.println(emp1.getPhones());
+		System.out.println(emp1.getAddress());
+		System.out.println(emp1.getCourses());
+	}
+
+}
