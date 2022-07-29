84768afb89afc6b71f6164361148aec4c9d7c6bf
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-07-29 08:34:57 +0530

    Constructor injection

---
+++ spring-core/src/main/java/com/springcore/ci/Certi.java
@@ -0,0 +1,18 @@
+package com.springcore.ci;
+
+public class Certi {
+	public String name;
+
+	public Certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+	
+	
+}
