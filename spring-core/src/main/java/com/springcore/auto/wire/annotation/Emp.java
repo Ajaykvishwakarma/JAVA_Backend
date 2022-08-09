234da52b61e8b100c754fbf7939e1bc9951a9c31
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-08-09 18:24:57 +0530

    @Autowired injection

---
+++ spring-core/src/main/java/com/springcore/auto/wire/annotation/Emp.java
@@ -0,0 +1,40 @@
+package com.springcore.auto.wire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Emp {
+	
+	@Autowired
+	private Address address;
+
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Emp(Address address) {
+		super();
+		this.address = address;
+		System.out.println("Inside the constructor");
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+	
+	
+	
+
+}
